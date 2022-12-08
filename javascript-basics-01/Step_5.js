19b5423924eedb32cf91bbbf2ed9e47680306116
malak-wahyb <malakwahyb537@gmail.com>
2022-12-08 10:20:14 +0200

    b01_step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+var num1=prompt("please enter number");
+var num2=prompt("please enter number");
+document.getElementById("first_number").value=num1;
+document.getElementById("second_number").value=num2;
+
+document.getElementById("validate").addEventListener("click", function() {
+    alert(num1*num2);
+  });
